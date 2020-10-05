d129c11a2b74178b7b83df946049709bac7462e6
Nina Loeterman <nina.loeterman@gmail.com>
2020-10-05 22:23:25 +0300

    testing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Player from "./Components/Player.jsx";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Player
+        video={"https://www.youtube.com/watch?v=A-yMz35wIxU&feature=youtu.be"}
+      />
+
+      {/* <Router>
         <Switch>
           <Route path="/suicide">
             <Player video={"https://www.youtube.com/watch?v=A-yMz35wIxU&feature=youtu.be"} />
           </Route>
-          {/* <Route path="/coming-back">
-            <Player video={comingBack} />
-          </Route> */}
         </Switch>
-      </Router>
+      </Router> */}
     </div>
   );
 }
