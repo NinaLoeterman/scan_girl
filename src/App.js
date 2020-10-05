f87edcb552e03d532ca18dc15427d9956ef1bd6d
Nina Loeterman <nina.loeterman@gmail.com>
2020-10-05 22:19:07 +0300

    build

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Player from "./Components/Player.jsx";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 
 function App() {
