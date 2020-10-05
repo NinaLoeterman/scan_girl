839dac1d98a8c69f71b5deee94d473ce24142eb2
Nina Loeterman <nina.loeterman@gmail.com>
2020-10-05 22:32:53 +0300

    routing back

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
 import "./App.css";
 import Player from "./Components/Player.jsx";
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Player
-        video={"https://www.youtube.com/watch?v=A-yMz35wIxU&feature=youtu.be"}
-      />
-
-      {/* <Router>
+      <Router>
         <Switch>
           <Route path="/suicide">
-            <Player video={"https://www.youtube.com/watch?v=A-yMz35wIxU&feature=youtu.be"} />
+            <Player
+              video={
+                "https://www.youtube.com/watch?v=A-yMz35wIxU&feature=youtu.be"
+              }
+            />
           </Route>
         </Switch>
-      </Router> */}
+      </Router>
     </div>
   );
 }
