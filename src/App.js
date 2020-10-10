2936a679fdcef5ae1d8ce0ffe8787a767f26c73b
Nina Loeterman <nina.loeterman@gmail.com>
2020-10-10 22:41:20 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Player from "./Components/Player.jsx";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
