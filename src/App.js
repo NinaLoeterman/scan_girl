8f432bc6e0e215ada9b046b4295297bc64bc1f1b
Nina Loeterman <nina.loeterman@gmail.com>
2020-10-10 21:47:54 +0300

    new routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React from "react";
 import "./App.css";
 import Player from "./Components/Player.jsx";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/suicide">suicide</Link>
+            </li>
+            <li>
+              <Link to="/coming-back">coming back</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
           <Route path="/suicide" component={Play} />
-      </Router>
+          <Route path="/coming-back" component={Play}>
+          </Route>
+          <Route path="/">
+            <div> landing page </div>            
+          </Route>
+        </Switch>
+      </div>
+    </Router>
     </div>
   );
 }
