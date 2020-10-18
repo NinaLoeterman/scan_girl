b11cc715d9e7169d53a692a8f3511e63ec93f4f1
Nina Loeterman <nina.loeterman@gmail.com>
2020-10-18 11:25:14 +0300

    styling modified

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+import "./HomePage.styles.scss";
+
+const text = {
+  hopfully:
+    "Hopefully you got here because you scanned a QR somewhere out there.",
+  welcome: "Welcome. And thank you for your curiosity.",
+};
 
 const HomePage = () => {
-    return ( 
-        <div>welcome</div>
-     );
-}
- 
+  return (
+    <div className="HomePage">
+      <div className={"HomePage_welcome_text_wrapper"}>
+        <div className={"HomePage_welcome_text"}>{text.hopfully}</div>
+        <div className={"HomePage_welcome_text"}>{text.welcome}</div>
+      </div>
+    </div>
+  );
+};
+
 export default HomePage;
